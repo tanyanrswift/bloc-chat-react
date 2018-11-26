b13e93246a56f30af37210e6b651f7e932530fce
Tanya Swift <tanyanicolerussi@gmail.com>
2018-11-25 19:23:52 -0700

    checkpoint first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import './components/RoomList';
+import RoomList from './components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyAUvEF3g0VofZ1r-qb4EUou6qrd9_vhUoo",
+  authDomain: "bloc-chat-react-441ac.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-441ac.firebaseio.com",
+  projectId: "bloc-chat-react-441ac",
+  storageBucket: "bloc-chat-react-441ac.appspot.com",
+  messagingSenderId: "392735703160"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Bloc Chat</h2>
         </header>
+        <RoomList />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+
+  render() {
+    return(
+      <section className="room-list">
+        <h2 id="room-list-title>">Bloc Chat</h2>
+        <div className="rooms">
+        {
+          this.state.rooms.map( (room) =>
+          <h3 id="room">{this.state.rooms}</h3>
+          )
+        }
+        </div>
+      </section>
+    );
+  }
+}
+
+export default RoomList;
